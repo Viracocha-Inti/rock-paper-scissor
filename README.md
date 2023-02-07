edc439fc97faa240abe050a734ea8e63ef62a4af
Viracocha-Inti <valenciasamir21@gmail.com>
2023-02-07 15:18:46 -0500

    Finish game code in javascript/Update README  Changes to be committed: 	modified:   README.md 	modified:   index.html 	modified:   javascript.js

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # rock-paper-scissor
 
-The goal of this project is to create the rock-paper-scissor game. 
-At first this game will only be playable in the console tab in
-Webtools.
+The goal of this project is to create the rock-paper-scissor game. This will test
+some foundational concepts in javascriptAt. This game will only be playable
+in the console tab in Devtools.
+\\
 Will add user interface at a later time and update the game.
 
 - Built with
-    - Semantic HTML5 markup
-    - Javascript
+  - Semantic HTML5 markup
+  - Javascript
--- javascript.js
+++ javascript.js
@@ -1,8 +1,4 @@
-//Selectors
-
-//Event Listeners
-
-//Functions
+//Function makes the computer output either rock, paper or scissors randomly for the game
 function getComputerChoice(ranNumber = Math.random()) {
   if (ranNumber <= 0.33) {
     return "Rock";
@@ -13,55 +9,74 @@ function getComputerChoice(ranNumber = Math.random()) {
   }
 }
 
-var userSelection = prompt("Do you choose rock, papper or scissors?!?", "");
-let uChoice = userSelection.toLowerCase();
+//Set intial user and computer score
 let userScore = 0;
 let compScore = 0;
 
-function playRound(uChoice, getComputerChoice) {
-  if (uChoice == "rock" && getComputerChoice == "Scissor") {
-    // userScore = userScore++;
+//Function sets and establishes the rules of the game
+function playRound(userSelection, getComputerChoice) {
+  if (userSelection.toLowerCase() == "rock" && getComputerChoice == "Scissor") {
     return "You Won";
-  } else if (uChoice == "papper" && getComputerChoice == "Rock") {
-    // userScore = userScore++;
+  } else if (
+    userSelection.toLowerCase() == "papper" &&
+    getComputerChoice == "Rock"
+  ) {
     return "You Won";
-  } else if (uChoice == "scissor" && getComputerChoice == "Papper") {
-    // userScore = userScore++;
+  } else if (
+    userSelection.toLowerCase() == "scissor" &&
+    getComputerChoice == "Papper"
+  ) {
     return "You Won";
-  } else if (uChoice == "rock" && getComputerChoice == "Rock") {
+  } else if (
+    userSelection.toLowerCase() == "rock" &&
+    getComputerChoice == "Rock"
+  ) {
     return "You Tied";
-  } else if (uChoice == "papper" && getComputerChoice == "Papper") {
+  } else if (
+    userSelection.toLowerCase() == "papper" &&
+    getComputerChoice == "Papper"
+  ) {
     return "You Tied";
-  } else if (uChoice == "scissor" && getComputerChoice == "Scissor") {
+  } else if (
+    userSelection.toLowerCase() == "scissor" &&
+    getComputerChoice == "Scissor"
+  ) {
     return "You Tied";
   } else {
-    // compScore = compScore++;
     return "You Lose";
   }
 }
+//Function sets up the game and calls on the game rules as well as the computer
+//choice to determine the winner
+function game() {
+  for (let i = 0; i < 5; i++) {
+    userSelection = prompt("Do you choose rock, papper or scissors?!?", "");
+    const computer = getComputerChoice();
+    const result = playRound(userSelection.toLowerCase(), computer);
+    if (result == "You Won") {
+      userScore++;
+      console.log(`You scored a point! Congratulations!!`);
+    } else if (result == "You Lose") {
+      compScore++;
+      console.log("You lost a point, better luck next time!!");
+    } else {
+      console.log("You tied");
+    }
+  }
+}
 
-// function game(result) {
-//   if (result == "You Won") {
-//     userScore = userScore + 1;
-//     console.log(
-//       `Congrats you scored a point, User score is ${userScore} and Computer score is ${compScore}`
-//     );
-//   } else if (result == "You Lose") {
-//     compScore = compScore + 1;
-//     console.log(
-//       `Sorry, you lost this round: User score is ${userScore} and Computer score is ${compScore}`
-//     );
-//   } else {
-//     console.log(
-//       `You tied, no points to either party!! User score is ${userScore} and Computer score is ${compScore}`
-//     );
-//   }
-// }
+game();
 
-console.log("User choice =", uChoice);
-const computer = getComputerChoice();
-console.log("Computer choice =", computer);
-const result = playRound(uChoice, computer);
-console.log(result);
-// const point = game(result);
-// console.log(point);
+if (userScore > compScore) {
+  console.log("You won, you destroyed the computer!!!");
+  console.log("Player score: ", userScore);
+  console.log("Computer score: ", compScore);
+} else if (compScore > userScore) {
+  console.log("You Lost. Computer takeover commencing!!");
+  console.log("Player score: ", userScore);
+  console.log("Computer score: ", compScore);
+} else if (userScore == compScore) {
+  console.log("You tied, you might want to try again!!");
+  console.log("Player score: ", userScore);
+  console.log("Computer score: ", compScore);
+}
